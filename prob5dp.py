21967919a13c8d535f00c8dca3ac0e167c0ccf98
viveganandan <middaysundonkey@gmail.com>
2018-03-25 17:15:23 -0700

    Added comments

--- prob5dp.py
+++ prob5dp.py
@@ -7,9 +7,11 @@ class Solution(object):
         beg = 0
         end = 0
         n = len(s)
+        # Using a n x n matrix to store the substring matches
         dp = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
         for i in range(n - 2, -1, -1):
             for j in range(i + 1, n):
+                # If ends match, the middle also has to match
                 if s[i] == s[j] and dp[i + 1][j - 1] > -1:
                     dp[i][j] = dp[i + 1][j - 1] + 2
                     if dp[i][j] > end - beg + 1:
